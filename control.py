7bd28bb8190c928ca544d7db18a08870ef06df38
pizzachip <pbyrne@razoyo.com>
2020-09-14 13:21:16 -0500

    initial commit with basic options

---
+++ control.py
@@ -0,0 +1,10 @@
+import module
+from time import sleep 
+
+x = 0
+
+while x < 10:
+    module.main()
+    x += 1
+    sleep(5)
+
---
+++ module.py
@@ -0,0 +1,8 @@
+import os
+
+def main():
+    print("mod_funct")
+    os.system("python premod.py")
+
+if __name__ == "__main__":
+    main()
